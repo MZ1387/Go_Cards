da614d5f46f8131a15119e59b05d82e0bd850be4
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-10-10 15:27:54 -0300

    Create a custom type deck

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
 func main() {
+	cards := deck{newCard(), "Ace of Base"}
+	cards = append(cards, "King of Hearts")
+	cards.print()
+}
 
+func newCard() string {
+	return "Five of diamonds"
 }
