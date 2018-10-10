9484c67186f5383e672e2fed118f74142cd72fe9
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-10-10 16:02:03 -0300

    Create a new deck

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace of Base"}
-	cards = append(cards, "King of Hearts")
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of diamonds"
 }
